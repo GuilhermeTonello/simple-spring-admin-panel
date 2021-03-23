178cfdc8e94b9fbe7902bff3c8a89682089067a4
Guilherme C. Tonello <guitonello@outlook.com.br>
2021-03-23 09:19:06 -0300

    Project created

---
+++ src/main/java/com/gui/adminpanel/AdminPanelApplication.java
@@ -0,0 +1,13 @@
+package com.gui.adminpanel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminPanelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdminPanelApplication.class, args);
+	}
+
+}
