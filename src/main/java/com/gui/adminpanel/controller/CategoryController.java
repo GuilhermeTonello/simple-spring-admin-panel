b74bb6ed489ea2e2fa3f10256c1f86b85ee082f3
Guilherme C. Tonello <guitonello@outlook.com.br>
2021-03-26 12:09:43 -0300

    Category controller and category home page

---
+++ src/main/java/com/gui/adminpanel/controller/CategoryController.java
@@ -0,0 +1,24 @@
+package com.gui.adminpanel.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.gui.adminpanel.repository.CategoryRepository;
+
+@Controller
+@RequestMapping("category")
+public class CategoryController {
+	
+	@Autowired
+	private CategoryRepository categoryRepository;
+	
+	@GetMapping({"", "home", "index"})
+	public ModelAndView categoryMainPage() {
+		ModelAndView modelAndView = new ModelAndView("admin/category/category-home");
+		return modelAndView;
+	}
+	
+}
